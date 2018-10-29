2b415153c1e3968d5db1128a23519b4ead52563f
zhweixi@sina.cn <zhweixi@sina.cn>
2018-10-29 18:21:08 +0800

    no message

--- app/app.js
+++ app/app.js
@@ -1,5 +1,36 @@
 App({
     data:{
         "host":"https://wx.laohoulundao.com/miniprogram/"
+    },
+    onLaunch:function(){
+
+        //1.检查本地是否有用户登录的信息
+        //app启动的时候检查用户是否登录
+
+
+        wx.login({
+            success:function(res){
+                if(res.code){
+                    console.log(res.code);
+                }else{
+                    console.log("error");
+                }
+            }
+        })
+
+
+        wx.getUserInfo({
+            success: function (res) {
+                console.log(res);
+                var userInfo = res.userInfo
+                var nickName = userInfo.nickName
+                var avatarUrl = userInfo.avatarUrl
+                var gender = userInfo.gender //性别 0：未知、1：男、2：女
+                var province = userInfo.province
+                var city = userInfo.city
+                var country = userInfo.country
+            }
+        })
     }
+
 })
